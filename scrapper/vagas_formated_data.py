124b27c00e4ef4d94f650e230786c7d71fb149f9
Yuri Queriquelli <yurirafael@outlook.com>
2021-08-16 22:04:47 -0300

    added -b parameter to choose between chrome or choromium

--- scrapper/vagas_formated_data.py
+++ scrapper/vagas_formated_data.py
@@ -7,17 +7,9 @@ from datetime import date, timedelta
 import os
 from unicodedata import normalize
 import csv
-
-#specify chrome locations
-driver_location = "../chromedriver"
-binary_location = "/usr/bin/chromium"
-
-#add options
-options = webdriver.ChromeOptions()
-options.add_argument('--start-fullscreen')
-options.binary_location = binary_location
-
-driver = webdriver.Chrome(executable_path=driver_location, options=options)
+#from connect import config #WIP connect to postgress 
+from configparser import ConfigParser
+import argparse
 
 def listToString(s):  
     
@@ -35,105 +27,129 @@ def listToString(s):
 def remover_acentos(txt):
     return normalize('NFKD', txt).encode('ASCII', 'ignore').decode('ASCII')
 
-#today
-today = date.today()
 
-#Open webdriver at site vagas.com
-urls = ["https://www.vagas.com.br/vagas-de-ti", "https://www.vagas.com.br/vagas-de-An%C3%A1lise-e-Desenvolvimento-de-Sistemas", "https://www.vagas.com.br/vagas-de-Com%C3%A9rcio-Exterior", "https://www.vagas.com.br/vagas-de-Gest%C3%A3o-Empresarial", "https://www.vagas.com.br/vagas-de-Gest%C3%A3o-de-Servi%C3%A7os", "https://www.vagas.com.br/vagas-de-Log%C3%ADstica", "https://www.vagas.com.br/vagas-de-Redes-de-Computadores"]
+def main():
 
-subject = 1
+    parser = argparse.ArgumentParser(description='choose some browser chrome or chromium')
+    parser.add_argument('-b', action='store', dest='browser', help='write chrome or chromium')
 
-row_list = [["Materia" , "Titulo", "link", "Descricao"]]
+    browser = parser.parse_args().browser
 
-for url in urls:
-    driver.get(url)
-   
-    if subject == 1:
-        text_subject = "Análise e Desenvolvimento de Sistemas"
-    elif subject == 2:
-        text_subject = "Análise e Desenvolvimento de Sistemas"
-    elif subject == 3:
-        text_subject = "Comércio Exterior"
-    elif subject == 4:
-        text_subject = "Gestão Empresarial"
-    elif subject == 5:
-        text_subject = "Gestão de Serviços"
-    elif subject == 6:
-        text_subject = "Logística Aeroportuária"
-    elif subject == 7:
-        text_subject = "Redes de Computadores"
+    if browser == 'chrome':
+        binary_location = "/usr/bin/google-chrome-stable" 
+    elif browser == 'chromium':
+        binary_location = "/usr/bin/chromium"
+    else:
+        print('choose a valid browser! chrome or chromium')
 
-    #Find the load button and click
-    while True:
-        try:
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            sleep(2)
-            button = driver.find_element_by_xpath('//*[@id="maisVagas"]')
-            button.click()
-            sleep(2)
-        except:
-            break
-        
-    #html parsing
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
+    #specify chrome locations
+    driver_location = "../chromedriver"
 
-    #find all jobs
-    vaga_odd = soup.findAll("li", {"class":"vaga odd"})
-    vaga_even = soup.findAll("li", {"class":"vaga even"})
-    vaga_total = vaga_even + vaga_odd
-    print(len(vaga_total))
+    #add options
+    options = webdriver.ChromeOptions()
+    options.add_argument('--start-fullscreen')
+    options.binary_location = binary_location
 
-    for script in soup(["script", "style"]):
-        script.extract()    # rip it out
+    driver = webdriver.Chrome(executable_path=driver_location, options=options)
 
-    #loops over all vaga_total
-    for vagas in vaga_total:
-        #extract job title
-        vaga_title = vagas.a["title"]
+    #today
+    today = date.today()
 
-        #extract job link
-        vaga_link = "https://www.vagas.com.br" + vagas.a["href"]
+    #Open webdriver at site vagas.com
+    urls = ["https://www.vagas.com.br/vagas-de-ti", "https://www.vagas.com.br/vagas-de-An%C3%A1lise-e-Desenvolvimento-de-Sistemas", "https://www.vagas.com.br/vagas-de-Com%C3%A9rcio-Exterior", "https://www.vagas.com.br/vagas-de-Gest%C3%A3o-Empresarial", "https://www.vagas.com.br/vagas-de-Gest%C3%A3o-de-Servi%C3%A7os", "https://www.vagas.com.br/vagas-de-Log%C3%ADstica", "https://www.vagas.com.br/vagas-de-Redes-de-Computadores"]
 
-        #extract job description     
-        driver.get(vaga_link)
-        sleep(1)
-        html_desc = driver.page_source
-        soup_vaga_desc = BeautifulSoup(html_desc, 'html.parser')
+    subject = 1
 
-        try:
-            container_vaga_desc = soup_vaga_desc.find("div","job-tab-content job-description__text texto")
-            vaga_desc_texto = container_vaga_desc.get_text()
-            vaga_desc_texto = vaga_desc_texto.replace(",", "-").replace(";","-")
-            vaga_desc_words = vaga_desc_texto.split(' ')
-            join_vaga_desc = listToString(vaga_desc_words)
-        
-        except:
-            join_vaga_desc  = "confidencial"
-        
-        try:
-            text_subject.replace(',', ' ') 
-            vaga_title.replace(',', ' ')
-            vaga_link.replace(',', ' ')
+    row_list = [["Materia" , "Titulo", "link", "Descricao"]]
 
-            actual_list = [text_subject, vaga_title, vaga_link, join_vaga_desc]   
-            row_list.append(actual_list.replace(";","-"))
+    for url in urls:
+        driver.get(url)
     
-        except:
-            continue
+        if subject == 1:
+            text_subject = "Análise e Desenvolvimento de Sistemas"
+        elif subject == 2:
+            text_subject = "Análise e Desenvolvimento de Sistemas"
+        elif subject == 3:
+            text_subject = "Comércio Exterior"
+        elif subject == 4:
+            text_subject = "Gestão Empresarial"
+        elif subject == 5:
+            text_subject = "Gestão de Serviços"
+        elif subject == 6:
+            text_subject = "Logística Aeroportuária"
+        elif subject == 7:
+            text_subject = "Redes de Computadores"
+
+        #Find the load button and click
+        while True:
+            try:
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                sleep(2)
+                button = driver.find_element_by_xpath('//*[@id="maisVagas"]')
+                button.click()
+                sleep(2)
+            except:
+                break
+            
+        #html parsing
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+
+        #find all jobs
+        vaga_odd = soup.findAll("li", {"class":"vaga odd"})
+        vaga_even = soup.findAll("li", {"class":"vaga even"})
+        vaga_total = vaga_even + vaga_odd
+        print(len(vaga_total))
+
+        for script in soup(["script", "style"]):
+            script.extract()    # rip it out
+
+        #loops over all vaga_total
+        for vagas in vaga_total:
+            #extract job title
+            vaga_title = vagas.a["title"]
+
+            #extract job link
+            vaga_link = "https://www.vagas.com.br" + vagas.a["href"]
+
+            #extract job description     
+            driver.get(vaga_link)
+            sleep(1)
+            html_desc = driver.page_source
+            soup_vaga_desc = BeautifulSoup(html_desc, 'html.parser')
+
+            try:
+                container_vaga_desc = soup_vaga_desc.find("div","job-tab-content job-description__text texto")
+                vaga_desc_texto = container_vaga_desc.get_text()
+                vaga_desc_texto = vaga_desc_texto.replace(",", "-").replace(";","-")
+                vaga_desc_words = vaga_desc_texto.split(' ')
+                join_vaga_desc = listToString(vaga_desc_words)
+            
+            except:
+                join_vaga_desc  = "confidencial"
+            
+            try:
+                text_subject.replace(',', ' ') 
+                vaga_title.replace(',', ' ')
+                vaga_link.replace(',', ' ')
+
+                actual_list = [text_subject, vaga_title, vaga_link, join_vaga_desc]   
+                row_list.append(actual_list.replace(";","-"))
+        
+            except:
+                continue
 
 
-with open('../db/vagas_formated_data.csv', 'a', newline='') as file:
-    writer = csv.writer(file, delimiter=';')
-    writer.writerows(row_list)
+    with open('../db/vagas_formated_data.csv', 'a', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerows(row_list)
 
 
-#close chrome
-driver.quit()
+    #close chrome
+    driver.quit()
 
-#close file
-file.close()
+    #close file
+    file.close()
 
 if __name__ == '__main__':
-    from doctest import testmod
-    testmod()
+    main()
--- scrapper/vagas_general_data.py
+++ scrapper/vagas_general_data.py
@@ -7,17 +7,8 @@ from datetime import date, timedelta
 import os
 from unicodedata import normalize
 import csv
-
-#specify chrome locations
-driver_location = "../chromedriver"
-binary_location = "/usr/bin/chromium"
-
-#add options
-options = webdriver.ChromeOptions()
-options.add_argument('--start-fullscreen')
-options.binary_location = binary_location
-
-driver = webdriver.Chrome(executable_path=driver_location, options=options)
+from configparser import ConfigParser
+import argparse
 
 def listToString(s):  
     
@@ -35,91 +26,112 @@ def listToString(s):
 def remover_acentos(txt):
     return normalize('NFKD', txt).encode('ASCII', 'ignore').decode('ASCII')
 
-if __name__ == '__main__':
-    from doctest import testmod
-    testmod()
+def main():
+    parser = argparse.ArgumentParser(description='choose some browser chrome or chromium')
+    parser.add_argument('-b', action='store', dest='browser', help='write chrome or chromium')
 
-#today
-today = date.today()
+    browser = parser.parse_args().browser
 
-#Open webdriver at site vagas.com
-urls = ["https://www.vagas.com.br/vagas-de-campinas", "https://www.vagas.com.br/vagas-de-s%C3%A3o-paulo",
-         "https://www.vagas.com.br/vagas-de-Indaiatuba", "https://www.vagas.com.br/vagas-em-sao-paulo"
-         , "https://www.vagas.com.br/vagas-de-sorocaba", "https://www.vagas.com.br/vagas-de-Piracicaba"]
+    if browser == 'chrome':
+        binary_location = "/usr/bin/google-chrome-stable" 
+    elif browser == 'chromium':
+        binary_location = "/usr/bin/chromium"
+    else:
+        print('choose a valid browser! chrome or chromium')
 
-row_list = [[ "Titulo", "link", "Descricao"]]
+    #specify chrome locations
+    driver_location = "../chromedriver"
 
-for url in urls:
-    driver.get(url)
+    #add options
+    options = webdriver.ChromeOptions()
+    options.add_argument('--start-fullscreen')
+    options.binary_location = binary_location
 
-    #Find the load button and click
-    while True:
-        try:
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            sleep(2)
-            button = driver.find_element_by_xpath('//*[@id="maisVagas"]')
-            button.click()
-            sleep(2)
-        except:
-            break
+    driver = webdriver.Chrome(executable_path=driver_location, options=options)
+
+    #today
+    today = date.today()
+
+    #Open webdriver at site vagas.com
+    urls = ["https://www.vagas.com.br/vagas-de-campinas", "https://www.vagas.com.br/vagas-de-s%C3%A3o-paulo",
+            "https://www.vagas.com.br/vagas-de-Indaiatuba", "https://www.vagas.com.br/vagas-em-sao-paulo"
+            , "https://www.vagas.com.br/vagas-de-sorocaba", "https://www.vagas.com.br/vagas-de-Piracicaba"]
+
+    row_list = [[ "Titulo", "link", "Descricao"]]
+
+    for url in urls:
+        driver.get(url)
+
+        #Find the load button and click
+        while True:
+            try:
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                sleep(2)
+                button = driver.find_element_by_xpath('//*[@id="maisVagas"]')
+                button.click()
+                sleep(2)
+            except:
+                break
+                
+        #html parsing
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+
+        #find all jobs
+        vaga_odd = soup.findAll("li", {"class":"vaga odd"})
+        vaga_even = soup.findAll("li", {"class":"vaga even"})
+        vaga_total = vaga_even + vaga_odd
+        print(len(vaga_total))
+
+        for script in soup(["script", "style"]):
+            script.extract()    # rip it out
+
+        #loops over all vaga_total
+        for vagas in vaga_total:
+            try:
+                #extract job title
+                vaga_title = vagas.a["title"]
+
+                #extract job link
+                vaga_link = "https://www.vagas.com.br" + vagas.a["href"]
+
+                #extract job description     
+                driver.get(vaga_link)
+                sleep(1)
+                html_desc = driver.page_source
+                soup_vaga_desc = BeautifulSoup(html_desc, 'html.parser')
+
             
-    #html parsing
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
+                container_vaga_desc = soup_vaga_desc.find("div","job-tab-content job-description__text texto")
+                vaga_desc_texto = container_vaga_desc.get_text()
+                vaga_desc_texto = vaga_desc_texto.replace(",", "-").replace(";","-")
+                vaga_desc_words = vaga_desc_texto.split(' ')
+                join_vaga_desc = listToString(vaga_desc_words)
+            
+            except:
+                continue
+            
+            try:
+                vaga_title.replace(',', ' ')
+                vaga_link.replace(',', ' ')
 
-    #find all jobs
-    vaga_odd = soup.findAll("li", {"class":"vaga odd"})
-    vaga_even = soup.findAll("li", {"class":"vaga even"})
-    vaga_total = vaga_even + vaga_odd
-    print(len(vaga_total))
-
-    for script in soup(["script", "style"]):
-        script.extract()    # rip it out
-
-    #loops over all vaga_total
-    for vagas in vaga_total:
-        try:
-            #extract job title
-            vaga_title = vagas.a["title"]
-
-            #extract job link
-            vaga_link = "https://www.vagas.com.br" + vagas.a["href"]
-
-            #extract job description     
-            driver.get(vaga_link)
-            sleep(1)
-            html_desc = driver.page_source
-            soup_vaga_desc = BeautifulSoup(html_desc, 'html.parser')
-
+                actual_list = [vaga_title, vaga_link, join_vaga_desc]   
+                row_list.append(actual_list.replace(";","-"))
         
-            container_vaga_desc = soup_vaga_desc.find("div","job-tab-content job-description__text texto")
-            vaga_desc_texto = container_vaga_desc.get_text()
-            vaga_desc_texto = vaga_desc_texto.replace(",", "-").replace(";","-")
-            vaga_desc_words = vaga_desc_texto.split(' ')
-            join_vaga_desc = listToString(vaga_desc_words)
-        
-        except:
-            continue
-        
-        try:
-            vaga_title.replace(',', ' ')
-            vaga_link.replace(',', ' ')
-
-            actual_list = [vaga_title, vaga_link, join_vaga_desc]   
-            row_list.append(actual_list.replace(";","-"))
-    
-        except:
-            continue
+            except:
+                continue
 
 
-with open('../db/reports/vagas_general_data.csv', 'a', newline='') as file:
-    writer = csv.writer(file, delimiter=';')
-    writer.writerows(row_list)
+    with open('../db/reports/vagas_general_data.csv', 'a', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerows(row_list)
 
 
+    #close chrome
+    driver.quit()
 
-#close chrome
-driver.quit()
+    #close file
+    file.close()
 
-#close file
-file.close()
+if __name__ == '__main__':
+    main()
