61c7604a0df5e255083161f6ab1625b9286ae1ab
Yuri Queriquelli <yurirafael@outlook.com>
2021-09-12 16:18:20 -0300

    New schema to create table

--- instance/create_table.py
+++ instance/create_table.py
@@ -14,35 +14,26 @@ def create_tables():
     """,
     """
         CREATE TABLE materias(
-            materia_id INT GENERATED ALWAYS AS IDENTITY,
-            materia_titulo VARCHAR(50) NOT NULL,
-            PRIMARY KEY(materia_id)
+            materia_id SERIAL PRIMARY KEY,
+            materia_titulo VARCHAR(50) NOT NULL
         )
         """,
         """
         CREATE TABLE vaga_formatada(
-            formatada_url VARCHAR(255) NOT NULL,
+            formatada_url VARCHAR(255) NOT NULL PRIMARY KEY,
             formatada_titulo VARCHAR NOT NULL,
-            formatada_desc VARCHAR NOT NULL,
-            materia_id INT NOT NULL,
-            PRIMARY KEY(materia_id),
-                FOREIGN KEY(materia_id)
-                    REFERENCES materias(materia_id)
-                    ON UPDATE CASCADE ON DELETE CASCADE
+            formatada_desc TEXT NOT NULL,
+            materia_id INT NOT NULL REFERENCES materias(materia_id)
         )
         """,
         """
         CREATE TABLE vaga_geral (
-            geral_url VARCHAR(255) NOT NULL,
+            geral_url VARCHAR(255) NOT NULL PRIMARY KEY,
             geral_titulo VARCHAR NOT NULL,
             geral_cargo VARCHAR NOT NULL,
             geral_desc VARCHAR NOT NULL,
             geral_data DATE,
-            materia_id INT NOT NULL,
-            PRIMARY KEY(materia_id),
-                FOREIGN KEY(materia_id)
-                    REFERENCES materias(materia_id)
-                    ON UPDATE CASCADE ON DELETE CASCADE
+            materia_id INT NOT NULL REFERENCES materias(materia_id)
         )
         """)
     conn = None
